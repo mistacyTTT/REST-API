09ddae244c3356a4ebb605b4c958971bdace1934
Stacy <mistacy40@gmail.com>
2025-04-16 08:43:32 +0300

    Setup the Environment, Server and MongoDB

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import http from  'http';
+import bodyparser from 'body-parser';
+import cookieparser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app= express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieparser());
+app.use(bodyparser.json());
+
+const server = http.createServer(app);
+
+server.listen(8000,() => {
+    console.log("server running on localhost http://localhost:8000/");
+});
+
+const MONGO_URL = 'mongodb+srv://mwanafunziv:pasword101@cluster0.w7mmry6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
