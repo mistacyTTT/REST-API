74c66bcb9311db56427431a2b36b66a03bbacb75
Stacy <mistacy40@gmail.com>
2025-04-17 11:01:05 +0300

    Allow a registered user to login

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
-import router from './router/rex';
+import router from './router/rex'
 
 const app= express();
 
