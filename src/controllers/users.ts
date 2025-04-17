2af3f7257a0415b50e3ff7fb78497df1e3f3beaf
Stacy <mistacy40@gmail.com>
2025-04-17 13:13:01 +0300

    .Allow the user to update their credentials

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { deleteUserByID, getUsers } from '../db/users';
+import { deleteUserByID, getUserByID, getUsers } from '../db/users';
 
 export const getAllUsers = async ( req: express.Request, res: express.Response) => {
     try{
@@ -20,6 +20,27 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const deletedUser = await deleteUserByID(id);
 
         return res.json(deletedUser);
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try{
+        const { id} = req.params;
+        const { username} = req.body;
+
+        if(!username){
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByID(id);
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json(user).end();
     }catch(error){
         console.log(error);
         return res.sendStatus(400);
